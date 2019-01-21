7437e7c7f3d09c32babad2ee089d0b6ba671bc9d
franklington <muehringer@live.com>
2019-01-21 22:29:08 +0100

    korrektur

--- add_film.php
+++ add_film.php
@@ -13,8 +13,8 @@ require 'head.php';
 
 
 <form action="execute_add_film.php" method="get">
-  <h1>Film hinufügen:</h1>
-    <div class="form-group col-md-6">
+  <h1>Film hinzufügen:</h1>
+    <div class="form-group">
       <label for="id">ID</label>
       <input type="text" name="id" class="form-control" id="id" placeholder="Film ID">
     </div>
@@ -30,7 +30,7 @@ require 'head.php';
     </div>
     <div class="form-group">
       <label for="datum">Release</label>
-      <input type="text" name="datum" class="form-control" id="datum" placeholder="TT-MMM-YY">
+      <input type="text" name="datum" class="form-control" id="datum" placeholder="YYYY-MM-DD">
     </div>
 
     <button type="submit" class="btn btn-primary">Hinzufügen</button>
